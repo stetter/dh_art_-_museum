028651ed4be032f9334d63277cd3d7d0dad3e90a
Roberto Stetter Netto <robertostetternetto@MacBook-Air-de-Roberto-Stetter.local>
2018-09-28 19:40:59 -0300

    telalogin

--- app/src/main/java/com/stetter/dhartmuseum/LoginActivity.java
+++ app/src/main/java/com/stetter/dhartmuseum/LoginActivity.java
@@ -3,11 +3,11 @@ package com.stetter.dhartmuseum;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_login);
     }
 }
