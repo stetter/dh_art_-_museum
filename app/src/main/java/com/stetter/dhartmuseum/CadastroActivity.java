493ea6e9d644d69c15ca6f85238228999a7acdd8
Erich <erichfalken95@gmail.com>
2018-09-29 11:06:19 -0300

    Atualização

--- app/src/main/java/com/stetter/dhartmuseum/CadastroActivity.java
+++ app/src/main/java/com/stetter/dhartmuseum/CadastroActivity.java
@@ -1,13 +1,27 @@
 package com.stetter.dhartmuseum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CadastroActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
