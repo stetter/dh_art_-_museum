a607ac0feed2381a340ef97a8048709fbd941e0c
Administrator <admin@example.com>
2018-12-12 11:09:03 -0200

    ajuste de layout

--- app/src/main/java/com/stetter/dhartmuseum/obras/ObrasActivity.java
+++ app/src/main/java/com/stetter/dhartmuseum/obras/ObrasActivity.java
@@ -3,14 +3,13 @@ package com.stetter.dhartmuseum.obras;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.stetter.dhartmuseum.R;
-import com.stetter.dhartmuseum.adapters.RecyclerViewObrasAdapter;
 import com.stetter.dhartmuseum.home.viewmodel.HomeViewModel;
 import com.stetter.dhartmuseum.interfaces.RecyclerViewOnItemClickListener;
 import com.stetter.dhartmuseum.model.Obras;
@@ -28,12 +27,13 @@ public class ObrasActivity extends AppCompatActivity implements RecyclerViewOnIt
     private RecyclerView.LayoutManager mLayoutManager;
     RecyclerViewListaObrasAdapter adapter;
     private HomeViewModel objectViewModel;
-    private RecyclerViewOnItemClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obras2);
+
+        setaRecyclerView();
 
         objectViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
 
@@ -42,29 +42,24 @@ public class ObrasActivity extends AppCompatActivity implements RecyclerViewOnIt
         objectViewModel.objectLiveData.observe(this, new Observer<List<Record>>() {
             @Override
             public void onChanged(@Nullable List<Record> records) {
-                for (int i = 0; i < records.size(); i++) {
-                    recordList.add(records.get(i));
-                }
-                setaRecyclerView();
+                adapter.update(records);
             }
         });
     }
 
     public void setaRecyclerView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerviewObras2);
-        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new GridLayoutManager(this, 2);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        adapter = new RecyclerViewListaObrasAdapter(this, recordList, listener);
+        adapter = new RecyclerViewListaObrasAdapter(recordList, this);
         mRecyclerView.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(Record record) {
-        Intent intent = new Intent(ObrasActivity.this, ObrasActivity.class);
+        Intent intent = new Intent(this, com.stetter.dhartmuseum.obras_detalhe.view.ObrasActivity.class);
         intent.putExtra("record", record);
         startActivity(intent);
-        //startActivity(new Intent(HomeActivity.this, ObrasActivity.class));
     }
-
 }
